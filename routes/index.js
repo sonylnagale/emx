42fa0e2cb3f3bb88ce33d3d4efc4e16184421edf
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2018-11-01 17:09:26 -0400

    incremental

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  switch (req.query) {
+  switch (req.query.q) {
     'Name':
       res.send('Sonyl Nagale');
     break;
     default:
       res.send("OK");
-
+      break;
   }
 });
 
